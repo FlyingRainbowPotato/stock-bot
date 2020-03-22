19204e427f809f0b281cc7a5a4cb781f33801e1c
FlyingRainbowPotato <patrickfalkeid@gmail.com>
2020-03-22 14:00:03 +0100

    added purchase amount element: <amount/>

--- lxmltest.py
+++ lxmltest.py
@@ -5,27 +5,33 @@ import datetime
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 parser = etree.XMLParser(remove_blank_text=True, remove_comments=True)
+tree = etree.parse("simulatorData.xml",parser) #loads in document with the parser previously defined
+root = tree.getroot() #root = <transactions/> basically
 
-tree = etree.parse("simulatorData.xml",parser)
-root = tree.getroot()
 buy = root[0]
 sell = root[1]
 
+buyIndex = len(buy)
+sellIndex = len(sell)
+
 etree.SubElement(buy, "stock")
-etree.SubElement(buy[0], "name")
-etree.SubElement(buy[0], "datetime")
-etree.SubElement(buy[0], "price")
-buy[0][0].text = "NIO"
-buy[0][1].text = now
-buy[0][2].text = "12.67"
+etree.SubElement(buy[buyIndex], "name")
+etree.SubElement(buy[buyIndex], "datetime")
+etree.SubElement(buy[buyIndex], "price")
+buy[buyIndex][0].text = "NIO"
+buy[buyIndex][1].text = now
+buy[buyIndex][2].text = "12.23"
 
 etree.SubElement(sell, "stock")
-etree.SubElement(sell[0], "name")
-etree.SubElement(sell[0], "datetime")
-etree.SubElement(sell[0], "price")
-sell[0][0].text = "NIO"
-sell[0][1].text = now
-sell[0][2].text = "12.67"
+etree.SubElement(sell[sellIndex], "name")
+etree.SubElement(sell[sellIndex], "datetime")
+etree.SubElement(sell[sellIndex], "price")
+sell[sellIndex][0].text = "NIO"
+sell[sellIndex][1].text = now
+sell[sellIndex][2].text = "13.33"
+
+
+
 
 filestuff = etree.tostring(tree, pretty_print=True, encoding='unicode')
 
